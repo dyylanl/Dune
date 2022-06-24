3a9407edcb475e0281a56d5de9bdfafe88458c6f
dyylanl <dledesma@fi.uba.ar>
2022-06-24 17:05:40 -0300

    gran refactor

--- CMakeFiles/dune-server_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/dune-server_autogen"
-  "dune-server_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/dune-server_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- Server/includes/defs.h
+++ Server/includes/defs.h
@@ -4,7 +4,7 @@
 // --------------------------- //
 #define MAX_CLIENTS_QUEUED 10
 
-#define MAP_PATH "/home/dypa/Dune/mapa5.yaml"
+#define MAP_PATH "/home/dypa/Dune/mapa3.yaml"
 
 // ------------
 #ifndef __INSTANCE_ID__
