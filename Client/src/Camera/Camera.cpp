a97cc91b1b1bb2c16cefd419a5c07bbb41862088
rluizaga <rluizaga@fi.uba.ar>
2022-07-04 01:28:15 -0300

    Se arregla textura del centro de construccion

--- Client/src/Camera/Camera.cpp
+++ Client/src/Camera/Camera.cpp
@@ -5,10 +5,10 @@
 #include "Camera.h"
 
 void Camera::update(SDL_Event &event) {
-    if(event.motion.x < 80 && m_viewBox.GetX() < 0) {
+    if(event.motion.x < 80 && m_viewBox.GetX() <= 0) {
         m_viewBox = m_viewBox + SDL2pp::Point(2, 0);
     }
-    if(event.motion.y < 60 && m_viewBox.GetY() < 0) {
+    if(event.motion.y < 60 && m_viewBox.GetY() <= 0) {
         m_viewBox = m_viewBox + SDL2pp::Point(0, 2);
     }
     if(event.motion.x > GAME_WIDTH - 80 && event.motion.x < GAME_WIDTH) {
