d4b788b4d5b82b5a91e1f304270a2fcb838834ef
Federico Gomez <fedemgp@gmail.com>
2022-07-09 18:02:12 -0300

    La camara estaba siendo muy sensible, cambio la logica para que reaccione ante evento de flechas de teclado presionadas

--- Client/src/Camera/Camera.cpp
+++ Client/src/Camera/Camera.cpp
@@ -5,17 +5,22 @@
 #include "Camera.h"
 
 void Camera::update(SDL_Event &event) {
-    if(event.motion.x < 80 && m_viewBox.GetX() <= 0) {
-        m_viewBox = m_viewBox + SDL2pp::Point(2, 0);
-    }
-    if(event.motion.y < 60 && m_viewBox.GetY() <= 0) {
-        m_viewBox = m_viewBox + SDL2pp::Point(0, 2);
-    }
-    if(event.motion.x > GAME_WIDTH - 80 && event.motion.x < GAME_WIDTH) {
-        m_viewBox = m_viewBox - SDL2pp::Point(2, 0);
-    }
-    if(event.motion.y > GAME_HEIGHT - 60) {
-        m_viewBox = m_viewBox - SDL2pp::Point(0, 2);
+    // Faltaria chequear que no me vaya de los limites del mapa
+    if (event.type == SDL_KEYDOWN) {
+        switch (event.key.keysym.sym) {
+            case SDLK_LEFT:
+                m_viewBox += SDL2pp::Point(5,0);
+                break;
+            case SDLK_RIGHT:
+                m_viewBox -= SDL2pp::Point(5,0);
+                break;
+            case SDLK_UP:
+                m_viewBox += SDL2pp::Point(0,5);
+                break;
+            case SDLK_DOWN:
+                m_viewBox -= SDL2pp::Point(0,5);
+                break;
+        }
     }
 }
 
