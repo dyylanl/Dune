81f5824db3cf52cb6aefb647a4edb6cebfa75e17
rluizaga <rluizaga@fi.uba.ar>
2022-07-17 21:18:54 -0300

    Se soluciona problema en la camara

--- Client/src/GameObject/Vehicle.cpp
+++ Client/src/GameObject/Vehicle.cpp
@@ -15,9 +15,9 @@ Vehicle::Vehicle(char textureID, SDL2pp::Point position, SDL2pp::Point size, int
 void Vehicle::processEvent(SDL_Event &event, BQueue<std::unique_ptr<CommandCL>> &queue, Camera &camera) {
     if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT) {
         SDL2pp::Rect shape = SDL2pp::Rect(m_position, m_size);
-        SDL2pp::Point point(event.motion.x, event.motion.y);
+        SDL2pp::Point point(event.motion.x - camera.getPosicion().GetX(), event.motion.y - camera.getPosicion().GetY());
         if (SDL_PointInRect(&point, &shape)) {
-            std::unique_ptr<CommandCL> command(new SelectCL(point - camera.getPosicion()));
+            std::unique_ptr<CommandCL> command(new SelectCL(point));
             std::cout << "Push command Select" << std::endl;
             queue.push(command);
         }
